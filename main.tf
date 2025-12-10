fdce2dd5e1bed907bbaeafb4bbdcd17716ffea62
Isidorio Alves Novaes <ianovaes@pop-os.localdomain>
2025-12-09 23:32:28 -0300

    chore: fix indent

--- main.tf
+++ main.tf
@@ -1,23 +1,24 @@
 modulo "ec2_instance" {
-    source = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-    name = "single-instance"
+  name = "single-instance"
 
-    instance_type = "t2.micro"
-    monitoring = false
-    vpc_security_group_ids = ["sg-06f76a53f08a418d4"]
-    subnet_id = "subnet-0bfb6070cab586136"
+  instance_type          = "t2.micro"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-06f76a53f08a418d4"]
+  subnet_id              = "subnet-0bfb6070cab586136"
 
-    tags = {
-        Terraform = "true"
-        Enviroment = "dev"
-    }
+  tags = {
+    Terraform  = "true"
+    Enviroment = "dev"
+    Name       = "Teste pipeline"
+  }
 }
 
 terraform {
-    backend "s3" {
-        buckend = "teste-repo-fullcycleteste-repo-ian-fullcycle"
-        key = "teste"
-        region = "us-east-1"
-    }
+  backend "s3" {
+    buckend = "teste-repo-fullcycleteste-repo-ian-fullcycle"
+    key     = "teste"
+    region  = "us-east-1"
+  }
 }
